734d6da7de27045f18d1db3e5e369af825f29ffb
Austin Hulen <ahulen08@gmail.com>
2019-12-17 08:52:14 -0800

    Create a container div and java function to load number of squares evenly into the container

--- myScript.js
+++ myScript.js
@@ -1,13 +1,13 @@
-let gridSize = 16;
+let gridSize = 20;
 function loadGrid(){
     for(let i = 0; i < gridSize; i++){
         for(let j = 0; j < gridSize; j++){
             let div = document.createElement('div');
             div.classList.add('grid');
+            div.style.width = ((850/gridSize)- .2) + "px";
+            div.style.height = ((850/gridSize) - .2) + "px";
             document.getElementById('container').appendChild(div);
         }
-        let jump = document.createElement('br');
-        document.getElementById('container').appendChild(jump);
 
     }
 }

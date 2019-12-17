d6c1aacf911860bb03e5ddf518ce50c3af644daa
Austin Hulen <ahulen08@gmail.com>
2019-12-17 10:02:25 -0800

    Add function to clear the Grid and inserted it into the beginning of the loadGrid function.

--- myScript.js
+++ myScript.js
@@ -1,5 +1,6 @@
-let gridSize = 20;
-function loadGrid(){
+loadGrid(16);
+function loadGrid(gridSize){
+    clearGrid();
     for(let i = 0; i < gridSize; i++){
         for(let j = 0; j < gridSize; j++){
             let div = document.createElement('div');
@@ -9,5 +10,11 @@ function loadGrid(){
             document.getElementById('container').appendChild(div);
         }
 
+    }
+}
+function clearGrid(){
+    const parent = document.getElementById("container");
+    while (parent.firstChild) {
+        parent.firstChild.remove();
     }
 }
